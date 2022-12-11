3aa7a74fc7cdd3e1c6a5bdf728ae8e09630ae67a
unknown <ana-claudia.lima@unesp.br>
2022-12-11 12:56:59 -0300

    Atualização exercício 6

--- codigo/exercicio_6.R
+++ codigo/exercicio_6.R
@@ -1,8 +1,10 @@
 # Instalando biblioteca readxl para importar para o R arquivos com extensão .xls ou .xlsx.
 if (!("readxl") %in% installed.packages()) install.packages("readxl")
+install.packages("qcc")
 
 # Carregando a biblioteca
 library(readxl)
+library(qcc)
 
 # Importando o arquivo do exercício 6
 dataframe.ex6 <- read_excel("./dados/exercicio6.xls",skip = 1, col_names = c("Qualidade", "N_pessoas"), col_types = c("text", "numeric"))
@@ -13,6 +15,8 @@ dataframe.ex6 <- read_excel("./dados/exercicio6.xls",skip = 1, col_names = c("Qu
 ex6.em.vetor <- c (dataframe.ex6$Qualidade)
 
 # criando um gráfico
+# Diagrama de Pareto
 
-ggplot(dataframe.ex6, aes( y= N_pessoas, x = Qualidade, fill = Qualidade))+
-  geom_bar (stat = "identity")
+pareto.chart(dataframe.ex6$N_pessoas, ylab = "Número de pessoas", cumperc = seq(0,100, by=10))
+
+
